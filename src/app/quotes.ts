8dee2045946506fe0eeb842aba0dbd19f1625dbf
gingerlauren <sagide.ginger@gmail.com>
2021-07-19 16:40:43 +0300

    new changes

---
+++ src/app/quotes.ts
@@ -0,0 +1,5 @@
+export class Quotes {
+  constructor(public id: number, public name: string, public author: string,
+    public upvote: number,  public downvote: number ,public person:String,public date:Date,) { }
+}
+
